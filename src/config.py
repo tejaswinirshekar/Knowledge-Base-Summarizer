f13fbc4e28ea7a0ec3b0c51313076fe03c378638
tejaswinirshekar <156666622+tejaswinirshekar@users.noreply.github.com>
2025-06-01 11:29:33 +0200

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,9 +1,9 @@
 import os
 
 # Pinecone configuration
-PINECONE_API_KEY = "pcsk_qDcxm_5MWGtm1wYNMNdbtGnx2fEBENN6D2PJtLYj24ojRQa7VmqT56BR27KRg6F5aetV4"
-PINECONE_INDEX_NAME = "knowledge-base"
-PINECONE_HOST = "https://knowledge-base-q2dp2fp.svc.aped-4627-b74a.pinecone.io"
+PINECONE_API_KEY =""
+PINECONE_INDEX_NAME = ""
+PINECONE_HOST = ""
 
 # Documents folder
 DOCS_PATH = os.path.join(os.path.dirname(__file__), "..", "docs")
